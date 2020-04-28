782ec38f573e65675d8b81692cb5681f879a4982
Gaston Mousques <mousques@hotmail.com>
2020-04-28 19:37:11 -0300

    minor changes

--- src/uy/edu/ort/cc/Main.java
+++ src/uy/edu/ort/cc/Main.java
@@ -3,7 +3,6 @@ package uy.edu.ort.cc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         ArduinoSketch anExampleScketch = new ArduinoSketch();
         anExampleScketch.setUp();
         anExampleScketch.loop();
