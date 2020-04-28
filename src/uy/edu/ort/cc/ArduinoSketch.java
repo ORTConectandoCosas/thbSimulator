51d08866b7eb9b69b0b325a0e9d936f44708efd1
Gaston Mousques <mousques@hotmail.com>
2020-04-28 18:02:47 -0300

    Added comments

--- src/uy/edu/ort/cc/ArduinoSketch.java
+++ src/uy/edu/ort/cc/ArduinoSketch.java
@@ -6,19 +6,28 @@ import com.google.gson.*;
 import java.util.concurrent.TimeUnit;
 
 public class ArduinoSketch{
+    /*
+       ThingsBoard parameters for MQTT
+     */
     private String thbServer = "tcp://demo.thingsboard.io:1883";
     private String publishTopic = "v1/devices/me/telemetry";
     private String clientId = "ASIMLATOR";
     private String deviceToken = "30Y3Hf2lC3N43UqWusNC";
     private MemoryPersistence persistence = new MemoryPersistence();
     private MqttConnectOptions connectOptions = new MqttConnectOptions();
-
     private MqttClient thbMqttClient = null;
+
+    /*
+        Sensor declaration
+     */
 
     private DTHSensor temperatureSensor = new DTHSensor();
 
+    // Arduino Setup
 
     public void setUp() {
+
+        //Connect to Thingsboar server
         connectOptions.setUserName(deviceToken);
         connectOptions.setMaxInflight(200);
         try {
@@ -28,23 +37,26 @@ public class ArduinoSketch{
             thbMqttClient.connect(connectOptions);
 
             System.out.println("Connecting to server");
-
-
         } catch (MqttException e) {
             System.out.println("Mqtt exception" + e.getMessage());
         }
 
     }
 
+    // Arduino Loop
     public void loop() {
         while(true) {
 
             try {
+
+                //Read sensor
                 float temperatureRead = temperatureSensor.readTemperature();
                 float humidityRead = temperatureSensor.readHumidity();
 
+                //Build json
                 String jsonString = "{\"temperature\" :" + Float.toString(temperatureRead) + "," + "\"humidity\":" + Float.toString(humidityRead) + "}";
 
+                //publish to telemetry topic
                 MqttMessage msg = new MqttMessage(jsonString.getBytes());
                 msg.setQos(0);
                 thbMqttClient.publish(publishTopic, msg);
@@ -52,6 +64,7 @@ public class ArduinoSketch{
 
                 // simulate a delay()
                 TimeUnit.MILLISECONDS.sleep(6000);
+
             } catch (MqttException e) {
                 System.out.println("Mqtt exception" + e.getMessage());
 
