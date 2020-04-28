1d05d1c4fd6fd5c87a6cd831b3b5108eee87de4f
Gaston Mousques <mousques@hotmail.com>
2020-04-28 15:19:26 -0300

    simple publish working

--- src/uy/edu/ort/cc/ArduinoSketch.java
+++ src/uy/edu/ort/cc/ArduinoSketch.java
@@ -4,15 +4,25 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class ArduinoSketch{
     private String thbServer = "tcp://demo.thingsboard.io:1883";
+    private String publishTopic = "v1/devices/me/telemetry";
     private String clientId = "ASIMLATOR";
-    private String token = "30Y3Hf2lC3N43UqWusNC";
+    private String deviceToken = "30Y3Hf2lC3N43UqWusNC";
     private MemoryPersistence persistence = new MemoryPersistence();
+    private MqttConnectOptions connectOptions = new MqttConnectOptions();
 
     private MqttClient thbMqttClient = null;
 
     public void setUp() {
+        connectOptions.setUserName(deviceToken);
+        connectOptions.setMaxInflight(200);
         try {
             thbMqttClient = new MqttClient(thbServer, clientId, persistence);
+            connectOptions.setMaxInflight(200);
+            //connectOptions.setCleanSession(true);
+            thbMqttClient.connect(connectOptions);
+
+            System.out.println("Connecting to server");
+
 
         } catch (MqttException e) {
             System.out.println("Mqtt exception" + e.getMessage());
@@ -21,6 +31,16 @@ public class ArduinoSketch{
     }
 
     public void loop() {
+
+        try {
+            String jsonString = "{\"temperature\" : 25.5,\"humidity\" : 69}";
+
+            MqttMessage msg = new MqttMessage(jsonString.getBytes());
+            msg.setQos(0);
+            thbMqttClient.publish(publishTopic, msg);
+        } catch (MqttException e) {
+            System.out.println("Mqtt exception" + e.getMessage());
+        }
 
     }
 
